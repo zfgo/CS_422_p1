b685708dabd20ae9b12688c527e0331646659fe7
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-04-30 23:38:36 -0700

    format changes

--- Django/mysite/zach_test/views.py
+++ Django/mysite/zach_test/views.py
@@ -9,54 +9,56 @@ from .models import Document, Task
 from .forms import DocumentForm, TaskForm
 
 def model_form_upload(request):
-   if request.method == 'POST':
-       form = TaskForm(request.POST)
-       file_form = DocumentForm(request.POST, request.FILES)
-       files_list = request.FILES.getList('document') # this is the field name in the model
-       if form.is_valid() and file_form.is_valid():
-           # set commit to false when calling save returns an object
-           # of the model that the modelForm is using, and does not
-           # save it to the DB
-           task_instance = form.save()
-           for f in files_list:
-               doc_instance = Document(document=f, task=task_instance)
-               doc_instance.save()
-               doc_instance.to_json() # convert the file to json
-               doc_instance.save() # save the Document model again
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        file_form = DocumentForm(request.POST, request.FILES)
+        files_list = request.FILES.getList('document') # this is the field name in the model
+        if form.is_valid() and file_form.is_valid():
+            # set commit to false when calling save returns an object
+            # of the model that the modelForm is using, and does not
+            # save it to the DB
+            task_instance = form.save()
+            for f in files_list:
+                doc_instance = Document(document=f, task=task_instance)
+                doc_instance.save()
+                doc_instance.to_json() # convert the file to json
+                doc_instance.save() # save the Document model again
 
 
-           return redirect('doc_list/')
+            return redirect('doc_list/')
 
 
-   else:
-       form = TaskForm()
-       file_form = DocumentForm()
-   return render(request, 'zach_test/upload.html', {
-       'form': form,
-       'file_form': file_form,
-   })
+    else:
+        form = TaskForm()
+        file_form = DocumentForm()
+    return render(request, 'zach_test/upload.html', {
+        'form': form,
+        'file_form': file_form,
+    })
 
+def get_file_metadata(request, file_id):
+    return HttpResponse("test (at file %s)." % file_id)
 
 
 
 def document_list(request):
-   documents = Document.objects.all()
-   return render(request, 'download.html', {'documents' : documents})
+    documents = Document.objects.all()
+    return render(request, 'download.html', {'documents' : documents})
 
 
 
 
 def download_file(request, file_id):
-   file = get_object_or_404(Document, pk=file_id)
+    file = get_object_or_404(Document, pk=file_id)
 
 
-   # Assuming the file field is named 'file_field' in your model
-   file_path = file.document.path
-   with open(file_path, 'rb') as f:
-       response = HttpResponse(f.read(), content_type='application/octet-stream')
-       response['Content-Disposition'] = 'attachment; filename=' + file.document.name
-   file_path2 = file.document2.path
-   with open(file_path, 'rb') as f:
-       response2 = HttpResponse(f.read(), content_type='application/octet-stream')
-       response2['Content-Disposition'] = 'attachment; filename=' + file.document2.name
-   return response
+    # Assuming the file field is named 'file_field' in your model
+    file_path = file.document.path
+    with open(file_path, 'rb') as f:
+        response = HttpResponse(f.read(), content_type='application/octet-stream')
+        response['Content-Disposition'] = 'attachment; filename=' + file.document.name
+    file_path2 = file.document2.path
+    with open(file_path, 'rb') as f:
+        response2 = HttpResponse(f.read(), content_type='application/octet-stream')
+        response2['Content-Disposition'] = 'attachment; filename=' + file.document2.name
+    return response
