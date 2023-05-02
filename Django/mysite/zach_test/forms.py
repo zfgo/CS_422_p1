d92ce0facf407b4a5875f2ad635eb80ea7d9a0b0
fleeber <erinliannestone@gmail.com>
2023-05-02 16:47:45 -0700

    changes to forms.py

--- Django/mysite/zach_test/forms.py
+++ Django/mysite/zach_test/forms.py
@@ -5,35 +5,39 @@ from .models import Document, Task
 
 # front end document form class
 class TaskForm(forms.ModelForm):
-    #def __init__(self, *args, **kwargs):
-    #   super().__init__(*args, **kwargs)
-    #   for key, field in self.fields.items():   # FLEEBER WANTS THESE 4 LINES TO STAY!
-    #       field.label = ""                    #they are currently disabled for easier debugging!!!!!
+    def __init__(self, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       for key, field in self.fields.items():   # FLEEBER WANTS THESE 4 LINES TO STAY!
+           field.label = ""                    #they are currently disabled for easier debugging!!!!!
     class Meta:
         model = Task 
         fields = ['task_desc','period', 'n_forecasts',] # ('task_desc', 'period', 'n_forecasts', )
         widgets = {
             'task_desc': forms.TextInput(attrs={'class':'form__input',
-				   'id':'form', 'placeholder':'Task description', 'autofocus':True}),
+				   'id':'t1', 'placeholder':'Task description', 'autofocus':True}),
+            'period': forms.TextInput(attrs={'class':'form__input',
+				   'id':'t2', 'placeholder':'Period', 'type':"number"}),
+            'n_forecasts': forms.TextInput(attrs={'class':'form__input',
+				   'id':'t3', 'placeholder':'Number of forecasts', 'type':"number"}),
         }
         """
          'period': forms.TextInput(attrs={'class':'form__input',
-				   'id':'form', 'placeholder':'Period', 'autofocus':True}),
+				   'id':'form', 'placeholder':'Period'}),
             'n_forecasts': forms.TextInput(attrs={'class':'form__input',
-				   'id':'form', 'placeholder':'n_forecasts', 'autofocus':True}),
+				   'id':'form', 'placeholder':'n_forecasts'}),
         """
         #TODO fields: [name/keywords,  forecasting period,  num of forecasts]
 
 class DocumentForm(forms.ModelForm):
-    #def __init__(self, *args, **kwargs):
-    #   super().__init__(*args, **kwargs)
-    #   for key, field in self.fields.items():   # FLEEBER WANTS THESE 4 LINES TO STAY!
-    #       field.label = ""                    #they are currently disabled for easier debugging!!!!!
+    def __init__(self, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       for key, field in self.fields.items():   # FLEEBER WANTS THESE 4 LINES TO STAY!
+           field.label = ""                    #they are currently disabled for easier debugging!!!!!
     class Meta:
         model = Document
         fields = ('document','document2') # 'document2')
         widgets = {
-            'document': forms.ClearableFileInput(attrs={'multiple': True}),
-            'document2': forms.ClearableFileInput(attrs={'multiple': True}),
+            'document': forms.ClearableFileInput(attrs={'multiple': True, 'id':'test', 'class':'hidden'}),
+            'document2': forms.ClearableFileInput(attrs={'multiple': True, 'id':'training', 'class':'hidden'}),
         }
         
